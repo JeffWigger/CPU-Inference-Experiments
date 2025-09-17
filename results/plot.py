8064f6d89e98688b3091e05552e8cd7bdbad68bf
Jeffrey <wiggerjeffrey@gmail.com>
2025-09-17 23:07:21 +0200

    fix plots

--- results/plot.py
+++ results/plot.py
@@ -26,24 +26,21 @@ b_sizes = ("Batch Size 1", "Batch Size 4", "Batch Size 8")
 x_pos = np.arange(len(b_sizes))
 
 
-def crate_plot(datasets, labels, width, name, title):
+def crate_plot(datasets, labels, name, title):
+    width = 1 / (len(datasets) + 1)
     fig, ax = plt.subplots(layout="constrained", figsize=(8, 5))
     colors = plt.cm.Pastel1(np.linspace(0, 0.1 * len(datasets), len(datasets)))
     for i, (ds, color) in enumerate(zip(datasets, colors, strict=False)):
-        offset = width * i
-        print(ds.t_total)
+        offset = width * (i + 1)
         ax.bar(x_pos + offset, ds.t_total, width, label=labels[i], color=color)
 
     ax.set_ylabel("Runtime (Seconds)")
     ax.set_title(title)
-    ax.set_xticks(x_pos + width, b_sizes)
+    ax.set_xticks(x_pos + 0.5, b_sizes)
     ax.legend(loc="upper left", ncols=len(datasets))
     ax.set_ylim(0, max([ds.t_total.max() for ds in datasets]) + 40)
 
     plt.savefig(name)
-
-
-width = 0.25
 
 
 # Compiled
@@ -52,13 +49,13 @@ r_compiled = df[df.experiment == "resnet_compiled"]
 r_compiled_exp = df[df.experiment == "resnet_export_compiled"]
 datasets = [r_no_opt, r_compiled, r_compiled_exp]
 labels = ["No Optimization", "Compiled", "Compiled and Exported"]
-crate_plot(datasets, labels, width, "resnet_compiled.svg", "Resnet Compiled")
+crate_plot(datasets, labels, "resnet_compiled.svg", "Resnet Compiled")
 
 b_no_opt = df_b[df_b.experiment == "bert_no_opt"]
 b_compiled = df_b[df_b.experiment == "bert_compiled"]
 datasets = [b_no_opt, b_compiled]
 labels = ["No Optimization", "Compiled"]
-crate_plot(datasets, labels, width, "bert_compiled.svg", "Bert Compiled")
+crate_plot(datasets, labels, "bert_compiled.svg", "Bert Compiled")
 
 
 # OpenVino
@@ -66,34 +63,34 @@ r_ovn = df[df.experiment == "resnet_ovn"]
 r_compiled_ovn = df[df.experiment == "resnet_compile_ovn"]
 datasets = [r_no_opt, r_compiled_ovn, r_ovn]
 labels = ["No Optimization", "Compiled OpenVino", "OpenVino"]
-crate_plot(datasets, labels, width, "resnet_ovn.svg", "Resnet OpenVino")
+crate_plot(datasets, labels, "resnet_ovn.svg", "Resnet OpenVino")
 
 b_compiled_ovn = df_b[df_b.experiment == "bert_compile_ovn"]
 datasets = [b_no_opt, b_compiled_ovn]
 labels = ["No Optimization", "Compiled OpenVino"]
-crate_plot(datasets, labels, width, "bert_ovn.svg", "Bert OpenVino")
+crate_plot(datasets, labels, "bert_ovn.svg", "Bert OpenVino")
 
 
 # IPEX
 r_compile_ipex = df[df.experiment == "resnet_compile_ipex"]
 datasets = [r_no_opt, r_compile_ipex]
 labels = ["No Optimization", "Compiled IPEX"]
-crate_plot(datasets, labels, width, "resnet_ipex.svg", "Resnet IPEX")
+crate_plot(datasets, labels, "resnet_ipex.svg", "Resnet IPEX")
 
 
 b_compiled_ipex = df_b[df_b.experiment == "bert_compile_ipex"]
 datasets = [b_no_opt, b_compiled_ipex]
 labels = ["No Optimization", "Compiled IPEX"]
-crate_plot(datasets, labels, width, "bert_ipex.svg", "Bert IPEX")
+crate_plot(datasets, labels, "bert_ipex.svg", "Bert IPEX")
 
 
 # ONNX
 r_onnx = df[df.experiment == "resnet_onnx"]
 datasets = [r_no_opt, r_onnx]
 labels = ["No Optimization", "Compiled ONNX"]
-crate_plot(datasets, labels, width, "resnet_onnx.svg", "Resnet ONNX")
+crate_plot(datasets, labels, "resnet_onnx.svg", "Resnet ONNX")
 
 b_onnx = df_b[df_b.experiment == "bert_onnx"]
 datasets = [b_no_opt, b_onnx]
 labels = ["No Optimization", "ONNX"]
-crate_plot(datasets, labels, width, "bert_onnx.svg", "Bert ONNX")
+crate_plot(datasets, labels, "bert_onnx.svg", "Bert ONNX")
